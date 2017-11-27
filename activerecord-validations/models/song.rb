25e3370bdf4cc60dcec3935703befba511d56162
ethunk <ethunk>
2017-11-27 18:42:02 -0500

    finished with lesson

--- activerecord-validations/models/song.rb
+++ activerecord-validations/models/song.rb
@@ -1,2 +1,10 @@
 class Song < ActiveRecord::Base
+  validates :year, numericality: true, length: { is: 4 }
+  validates :title, presence: true
+  validates :artist, presence: true
+  validates :album, presence: true
+  validates :track_number
+  validates :genre
+  validates :length_in_seconds, presence: true, numericality: true
+  validates :image
 end
